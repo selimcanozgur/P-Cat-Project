b8c1a2d1f512710a1df4e0d2f4913914cb57e824
Selim <webisdeveloper@gmail.com>
2023-03-27 14:33:22 +0300

    all file add

--- app.js
+++ app.js
@@ -1,14 +1,45 @@
+const mongoose = require ('mongoose');
 const express = require('express');
 const path = require('path');
+const ejs = require('ejs');
+const Photo = require ('./modules/Photo');
+
+
 const app = express();
 
-app.use(express.static('public'));
+mongoose.connect('mongodb://127.0.0.1/my_database');
 
-app.get('/',(req, res)=>{
-    res.sendFile(path.resolve(__dirname, 'temp/index.html'));
-})
+// TEMPLATE ENGINE
+app.set('view engine', 'ejs');
+
+// MIDDLEWARES
+app.use(express.static('public'));
+app.use(express.urlencoded({extended:true}))
+app.use(express.json());
+
+//ROUTES
+app.get('/', async (req, res) => {
+  const photos = await Photo.find({})
+  res.render('index', {
+    photos
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/add', (req, res) => {
+  res.render('add');  
+});
+
+app.post('/photos', async (req, res) => {
+ await Photo.create(req.body);
+  res.redirect('/');
+});
+
 
 const port = 3000;
-app.listen(port,()=>{
-    console.log(`Server port ${port}'de başlatıldı.`);
-})
+app.listen(port, () => {
+  console.log(`Server port ${port}'de başlatıldı.`);
+});
